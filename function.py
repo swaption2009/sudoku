734f4c9097a032380f9dff8caa3e60830dcfc4d3
swaption2009 <swaption2009@gmail.com>
2017-02-28 17:13:59 -0800

    revert

--- function.py
+++ function.py
@@ -3,30 +3,21 @@ from utils import *
 def search(values):
     "Using depth-first search and propagation, try all possible values."
     # First, reduce the puzzle using the previous function
-    # print("values before reduce puzzle: ", values)
     values = reduce_puzzle(values)
-    print("values after reduce puzzle: ", values)
     if values is False:
         return False ## Failed earlier
     if all(len(values[s]) == 1 for s in boxes):
-        print("solved puzzle: ", values)
         return values ## Solved!
     # Choose one of the unfilled squares with the fewest possibilities
     n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
-    print("n: ", n)
-    print("s: ", s)
-    print("values[s]: ", values[s])
     # Now use recurrence to solve each one of the resulting sudokus, and
     for value in values[s]:
         new_sudoku = values.copy()
-        print("new sudoku: ", new_sudoku)
         new_sudoku[s] = value
-        print("new sudoku[s]: ", new_sudoku[s])
         attempt = search(new_sudoku)
         if attempt:
-            # print("end")
             return attempt
 
-# display(search(grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..')))
+display(search(grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..')))
 
 display(search(grid_values('4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......')))
--- utils.py
+++ utils.py
@@ -9,6 +9,7 @@ row_units = [cross(r, cols) for r in rows]
 col_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
 unitlist = row_units + col_units + square_units
+<<<<<<< HEAD
 
 # diagonal sudoku
 diagonal1 = [a[0]+a[1] for a in zip(rows, cols)]
@@ -30,6 +31,18 @@ print("diagonal2: ", diagonal2)
 print("unitlist: ", unitlist)
 print("units: ", units)
 print("peers: ", peers)
+=======
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+
+# print(boxes)
+# print(row_units[0])
+# print(col_units[0])
+# print(square_units[5])
+# print(unitlist)
+# print(units)
+# print(peers)
+>>>>>>> parent of c42c551... diagonal sudoku and naked twin
 
 
 def display(values):
@@ -103,6 +116,7 @@ def only_choice(values):
                 values[dplaces[0]] = digit
     return values
 
+
 def naked_twin(values):
     # step 1: find values[s] with 2 digits
     twin_digits = []
@@ -140,26 +154,17 @@ def reduce_puzzle(values):
     """
     stalled = False
     while not stalled:
-        print("original values: ", values)
         # Check how many boxes have a determined value
         solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
-        print("number of solved values before: ", solved_values_before)
         # Use the Eliminate Strategy
         values = eliminate(values)
-        print("solved values after elimination: ", values)
         # Use the Only Choice Strategy
         values = only_choice(values)
-        print("solved values after only_choice: ", values)
-        # Use Naked Twin Strategy
-        values = naked_twin(values)
-        print("solved values after naked_twin: ", values)
         # Check how many boxes have a determined value, to compare
         solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
-        print("number of solved values after: ", solved_values_after)
         # If no new values were added, stop the loop.
         stalled = solved_values_before == solved_values_after
         # Sanity check, return False if there is a box with zero available values:
         if len([box for box in values.keys() if len(values[box]) == 0]):
             return False
-    print("values from reduce puzzle function: ", values)
     return values
